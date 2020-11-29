04299562c717d84bf4d611eb3e99faf8ec88aba2
user <user@DESKTOP-07SQP7N>
2020-11-29 17:29:49 -0500

    Iniciando proyecto

---
+++ src/main/java/pe/edu/tecsup/jwa/petclinic/controller/PetController.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.jwa.petclinic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class PetController {
+@GetMapping("welcome")
+String welcome() {
+return "Welcome to Pet Store";
+}
+}
